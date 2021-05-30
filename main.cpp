bb1baeffdb58933f80081b5985b384a1d9a94fa6
yu_rafed <yu_rafed@mail.ru>
2021-05-30 18:11:00 +0300

    2

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "sdt.h"
+#include <iostream>
+int main()
+{
+    int a, b, c;
+    std::cout << "Enter A and B:";
+    std::cin >> a >>b;
+    c = a + b;
+    std::cout << "Sum: " << c ;
+}
+//прапрам
