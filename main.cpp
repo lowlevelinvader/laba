4da1cfc8e4a8d3a44b17649c6216154361491b0a
yu_rafed <yu_rafed@mail.ru>
2021-05-31 00:21:02 +0300

    Multiplication

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 int main()
 {
-    int a, b, c,d;
+    int a, b, c,d,f;
     std::cout << "Enter A and B:";
     std::cin >> a >>b;
     c = a + b;
     d = a - b;
+    f = a * b;
     std::cout << "Sum: " << c ;
     std::cout << "Dif: " << d ;
+    std::cout << "Multi: " << f ;
 }
 //Программа является учебной
