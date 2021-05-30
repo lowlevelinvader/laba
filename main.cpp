8c1377b0a297635ed0c67c83d9519bf07ac34b44
yu_rafed <yu_rafed@mail.ru>
2021-05-31 00:14:50 +0300

    Деление

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 int main()
 {
-    int a, b, c,d;
+    int a, b, c,d,e;
     std::cout << "Enter A and B:";
     std::cin >> a >>b;
     c = a + b;
     d = a - b;
+    e= a / b;
     std::cout << "Sum: " << c ;
     std::cout << "Dif: " << d ;
+    std::cout << "Div: " << e ;
 }
 //Программа является учебной
