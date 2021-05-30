2510521b196dff32b6c01e541d61cb3698abd785
yu_rafed <yu_rafed@mail.ru>
2021-05-31 00:37:18 +0300

    merged

--- main.cpp
+++ main.cpp
@@ -2,22 +2,16 @@
 #include <iostream>
 int main()
 {
-<<<<<<< HEAD
     int a, b, c,d,f,e;
-=======
->>>>>>> Division
     std::cout << "Enter A and B:";
     std::cin >> a >>b;
     c = a + b;
     d = a - b;
-<<<<<<< HEAD
     f = a * b;
     std::cout << "Sum: " << c ;
     std::cout << "Dif: " << d ;
     std::cout << "Multi: " << f ;
-=======
     e= a / b;
     std::cout << "Div: " << e ;
->>>>>>> Division
 }
 //Программа является учебной
