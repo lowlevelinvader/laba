5e6afdc4786b70ad22ff04ce5ee3eb95933bc560
yu_rafed <yu_rafed@mail.ru>
2021-05-30 22:17:17 +0300

    Пустая программа

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include "sdt.h"
-#include <iostream>
+
 int main()
 {
-    int a, b, c;
-    std::cout << "Enter A and B:";
-    std::cin >> a >>b;
-    c = a + b;
-    std::cout << "Sum: " << c ;
+    
 }
 
